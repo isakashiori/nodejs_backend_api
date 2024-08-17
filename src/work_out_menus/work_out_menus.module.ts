9ffdd456d72dcd180295bf86ce6c7538d542f518
isakashiori <isslamdunk0000@gmail.com>
2024-08-17 18:25:05 +0900

    feat: prismaでwork_out_menuモデルを操作できるようにした

--- src/work_out_menus/work_out_menus.module.ts
+++ src/work_out_menus/work_out_menus.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
 import { WorkOutMenusService } from './work_out_menus.service';
+import { WorkOutMenusController } from './work_out_menus.controller';
+import { PrismaService } from '../prisma.service';
 
 @Module({
-  providers: [WorkOutMenusService]
+  controllers: [WorkOutMenusController],
+  providers: [WorkOutMenusService, PrismaService]
 })
 export class WorkOutMenusModule {}
--- src/work_out_menus/work_out_menus.service.ts
+++ src/work_out_menus/work_out_menus.service.ts
@@ -1,4 +1,47 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma.service';
+import { WorkOutMenu, Prisma } from '@prisma/client';
 
 @Injectable()
-export class WorkOutMenusService {}
+export class WorkOutMenusService {
+  constructor(private prisma: PrismaService) {}
+
+  async findAll(): Promise<WorkOutMenu[]> {
+    const posts = await this.prisma.workOutMenu.findMany();
+    return posts;
+  }
+
+  async create(data: Prisma.WorkOutMenuCreateInput): Promise<WorkOutMenu> {
+    return this.prisma.workOutMenu.create({
+      data,
+    });
+  }
+
+  async findOne(id: number): Promise<WorkOutMenu> {
+    return await this.prisma.workOutMenu.findUnique({
+      where: {
+        id: id,
+      },
+    })
+  }
+
+  async update(
+    id: number,
+    data: Prisma.WorkOutMenuUpdateInput
+  ): Promise<WorkOutMenu> {
+    return this.prisma.workOutMenu.update({
+      data,
+      where: {
+        id: id,
+      },
+    });
+  }
+
+  async delete(id: number): Promise<WorkOutMenu> {
+    return await this.prisma.workOutMenu.delete({
+      where: {
+        id: id,
+      },
+    })
+  }
+}
